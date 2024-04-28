bbca2d87163d826df0d2f0ae9d811c6d2b7da744
albertchae <217050+albertchae@users.noreply.github.com>
2024-04-28 16:17:13 -0700

    Update mirroring action
    We want slightly different behavior for our fork of the mirror action
    - we mostly only care about mirroring the master branch
    - don't force push so that divergence with upstream requires manual intervention
    - s/mirror/upstream for more consistency with our terminology. The GitHub repo is the mirror in our case, however we are just keeping that as `origin` for now.

--- action.sh
+++ action.sh
@@ -9,6 +9,6 @@ fi
 
 git config --global --add safe.directory /github/workspace
 git clone --bare "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" . || exit 1
-git remote add --mirror=fetch mirror "${REMOTE}" || exit 1
-git fetch mirror +refs/heads/*:refs/remotes/origin/* || exit 1
-git push --force --mirror --prune origin || exit 1
+git remote add --mirror=fetch upstream "${REMOTE}" || exit 1
+git fetch upstream || exit 1
+git push origin master || exit 1
